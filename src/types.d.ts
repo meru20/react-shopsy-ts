4512821535ef1117228832f4f2db0445e2557fa0
meru20 <merhawitgubsa@gmail.com>
2021-04-23 15:28:10 -0700

    feat(Products): adding products and pulling in to homepage

---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+/**
+ * {
+                    id:1,
+                    title:'...',
+                    price:'...',
+                    category:'...',
+                    description:'...',
+                    image:'...'
+                },
+ * 
+ */
+
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+    category: string;
+    description: string;
+    image: string;
+
+}
