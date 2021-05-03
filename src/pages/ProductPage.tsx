4f08978b15d0f07b0400847b9bf18184ed01e20c
meru20 <merhawitgubsa@gmail.com>
2021-05-03 14:54:20 -0700

    feat(context): endpoint for single product

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,9 +1,19 @@
+import {useContext, useEffect} from 'react';
+import {GlobalContext} from '../context/GlobalContext';
+import {useParams} from 'react-router-dom';
 const ProductPage = () => {
+    const {product,getSingleProduct} = useContext(GlobalContext);
+    const {productId} = useParams< {productId: string}>();
+    // console.log('params', params);
+    useEffect (() => {
+        getSingleProduct(+productId);
+
+    },[productId])
     return (
         <div id='product'>
             <div className='row text-center'>
                 <div className='col'>
-                    <h2>Product page</h2>
+                    <h2>Product page{product?.title}</h2>
                 </div>
             </div>
         </div>
