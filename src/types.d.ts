a6da5c064bfd04aeede741af229167e725d161d1
meru20 <merhawitgubsa@gmail.com>
2021-05-11 22:16:59 -0700

    feat(cartContext):add to cart functionality

--- src/types.d.ts
+++ src/types.d.ts
@@ -23,9 +23,15 @@ type Product = {
 type InitialStateType={
     products: Product[];
     cart: Product[];
+    cart: CartItem[];
     product: Product | undefined;
     getProducts: () => void;
     getSingleProduct: (productId:number) => void ;
     addToCart:(product: Product) =>  void;
     is_loading: boolean;
+}
+type CartItem = {
+    id: number;
+    quantity: number;
+    product:Product;
 }
